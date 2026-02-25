c43a779bd8aed5851f520050d62c6c646a04a56c
Surya-palanisamy <suryapalanisamy2811@gmail.com>
2026-02-25 12:00:09 +0530

    seo added and robots.txt added

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://nano-ink.vercel.app",
+  generateRobotsTxt: true,
+
+  // Optional but good for SEO
+  changefreq: "weekly",
+  priority: 0.7,
+  sitemapSize: 5000,
+};
